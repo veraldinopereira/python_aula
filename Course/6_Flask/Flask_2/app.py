52361314b9120fc04fe3d418565ed6f022584894
veraldinopereira <veraldino.pereira@gmail.com>
2024-05-06 14:54:38 -0300

    06-05-2054

--- Course/6_Flask/Flask_2/app.py
+++ Course/6_Flask/Flask_2/app.py
@@ -1,1 +1,18 @@
-# resgatar o names de cada campo 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    dados_concatenados = None 
+    if request.method == 'POST':
+        nome = request.form['nome']
+        idade = request.form['idade']
+        sexo = request.form['sexo']
+        documento = request.form['documento']
+        cidade = request.form['cidade']
+        dados_concatenados = f"Nome: {nome}, Idade: {idade}, Sexo: {sexo}, Documento: {documento}, Cidade: {cidade}"
+    return render_template('index.html', dados_concatenados=dados_concatenados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
