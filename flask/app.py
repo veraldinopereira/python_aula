4ff179d0c2aafe6856972ebac4a74d91e1204f39
veraldinopereira <veraldino.pereira@gmail.com>
2024-06-07 20:01:45 -0300

    07-06-2024

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+usuarios = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -9,8 +11,11 @@ def index():
 @app.route('/', methods=['POST'])
 def cadastro():
     nome = request.form['nome_py']
-    print(nome)
-    return render_template('index.html')
+    idade = request.form['idade_py']
+    usuario = {"nome":nome, "idade": idade}
+    usuarios.append(usuario)
+    print(usuarios)
+    return render_template('index.html', resultado = usuarios)
 
 if __name__ == '__main__':
     app.run(debug=True)
