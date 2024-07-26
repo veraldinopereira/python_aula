5d2be7153c665e26f21c72e4bb2cb2c37adaad27
veraldinopereira <veraldino.pereira@gmail.com>
2024-07-25 21:31:08 -0300

    update-25-07-2024

--- api/app.py
+++ api/app.py
@@ -18,7 +18,7 @@ def buscar_id(id):
             return jsonify(element)
     return jsonify({'msg':'no existe usuario com esse ID'})
 
-@app.route('/api/cadastrar', methods=['POST'])
+@app.route('/api/usuarios/cadastrar', methods=['POST'])
 def cadastrar():
     new_user = request.get_json()
     with open ('data.json', encoding='utf-8') as file:
@@ -37,5 +37,31 @@ def cadastrar():
         json.dump(data, file, indent=4, ensure_ascii=False)
     return jsonify({"msg":"usuario cadastrado com sucesso"})    
 
+@app.route("/api/usuarios/<int:id>", methods=["Put"])
+def update_user(id):
+    update_data = request.get_json()
+    with open("data.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    for element in data['usuarios']:
+        if element['id'] == id:
+            element.update(update_data)
+            with open('data.json', 'w', encoding='utf-8') as json_file:
+                json.dump(data, json_file)
+            return jsonify({'message': 'Usuario atualizado com sussa'})
+    return jsonify({'message': 'Usuario not found'}), 404   
+
+@app.route("/api/usuarios/apagar/<int:id>", methods=['DELETE'])
+def delete_user(id):
+    with open('data.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    for element in data['usuarios']:
+        if element['id'] == id:
+            data['usuarios'].remove(element)
+            with open('data.json', 'w', encoding='utf-8') as json_file:
+                json.dump(data, json_file)
+            return jsonify({'message':'usuario deletado com sussa'})
+    return jsonify({'message': 'usuario not foun'}), 404
+
+        
 if __name__ == '__main__':
     app.run(debug=True)
